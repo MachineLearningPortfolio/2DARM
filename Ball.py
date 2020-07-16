d999a8b2ea5616dddd8701e4d45c025a3a268d25
felixwiberg <felixv@student.chalmers.se>
2020-07-16 18:51:01 +0200

    added animation

--- Ball.py
+++ Ball.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class Ball:
 
@@ -24,16 +25,37 @@ class Ball:
 
 dt = 0.01
 
-ball = Ball(vx=3, vy=6)
-plt.figure()
-plt.ion()
 
-while ball.getY() > 0.999:
-    ball.update(dt)
-    plt.scatter(ball.getX(), ball.getY())
-    plt.axis([0, 5, 0, 5])
-    plt.draw()
-    plt.pause(0.01)
-    plt.cla()
-plt.ioff()
-plt.show()
+class AnimatedScatter(object):
+    def __init__(self):
+        self.stream = self.data_stream()
+        self.fig, self.ax = plt.subplots()
+        self.ax.plot([1,3],[1.0, 1.0], c='k')
+        self.ax.plot([5,6],[1.0, 1.0], c='k')
+        self.ani = animation.FuncAnimation(self.fig, self.update, interval=10,
+                                          init_func=self.setup_plot, blit=True)
+        self.ball = Ball(vx=3, vy=6)
+
+    def setup_plot(self):
+        x, y = next(self.stream).T
+        self.scat = self.ax.scatter(x, y, vmin=0, vmax=1,
+                                    cmap="jet", edgecolor="k")
+        self.ax.axis([1, 6, 0.5, 3])
+        return self.scat,
+
+    def data_stream(self):
+        while True:
+            self.ball.update(dt)
+            yield np.c_[self.ball.getX(), self.ball.getY()]
+            if self.ball.getY() < 1:
+                self.ball.setState(1, 1, np.random.randint(0.1,7), np.random.randint(0.1,7))
+
+    def update(self, i):
+        data = next(self.stream)
+        self.scat.set_offsets(data)
+        return self.scat,
+
+
+if __name__ == '__main__':
+    a = AnimatedScatter()
+    plt.show()
