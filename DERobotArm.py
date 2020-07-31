bbb946b0e48289c7fd3656c8ceacc4a31974d6dd
felixwiberg <felix@felix.localdomain>
2020-07-31 14:06:43 +0200

    added animation

--- DERobotArm.py
+++ DERobotArm.py
@@ -7,15 +7,15 @@ class RobotArm:
         np.random.seed()
         self.dt = 0.01
         self.length = length
-        self.theta = theta
-        self.omega = omega
-        self.alpha = alpha
+        self.theta = np.random.rand()*2*np.pi
+        self.omega = (np.random.rand()-0.5)*8*np.pi
+        self.alpha = (np.random.rand()-0.5)*8*np.pi
         self.mass = mass
         self.inertia = 1/3*self.mass*self.length**2 #rod
         self.has_ball = has_ball
-        self.position = [-self.length*np.cos(theta), -self.length*np.sin(theta)]
-        self.velocity = [self.length*omega*np.sin(theta), -self.length*omega*np.cos(theta)]
-        self.target = np.random.rand()*10+1
+        self.position = [-self.length*np.cos(self.theta), -self.length*np.sin(self.theta)]
+        self.velocity = [self.length*self.omega*np.sin(self.theta), -self.length*self.omega*np.cos(self.theta)]
+        self.target = 7. #np.random.rand()*10+1
 
     def get_state(self):
         return np.array([self.theta, self.omega, self.alpha, self.target], dtype=np.float32)
@@ -24,27 +24,33 @@ class RobotArm:
         self.__init__()
         return self.get_state()
 
-    def step(self, action):
+    def step(self, action, plot, i):
         d_acc = 0
         if action == 0:
             d_acc = 0
         elif action == 1:
-            d_acc = 0.01
+            d_acc = 5
         elif action == 2:
-            d_acc = -0.01
+            d_acc = -5
         elif action == 3:
             while self.position[1] > -self.length:
                 self.velocity = [self.velocity[0], self.velocity[1] - 9.82 * self.dt]
                 self.position = [self.position[0] + self.velocity[0] * self.dt,
                                  self.position[1] + self.velocity[1] * self.dt]
-            return [self.get_state(), 1/np.abs(self.target - self.position[0]), True, {}]
+                if plot:
+                    plt.scatter(self.position[0], self.position[1])
+            return [self.get_state(), 1/np.abs(self.target - self.position[0]), False, True]
 
         self.alpha = self.mass*9.82*self.length*np.cos(self.theta)/(2*self.inertia)-self.omega/10+d_acc
         self.omega += self.alpha*self.dt
         self.theta += self.omega*self.dt
+        self.theta = self.theta % (2*np.pi)
+        if self.theta < 0:
+            self.theta += 2*np.pi
+        
         self.position = [-self.length*np.cos(self.theta), -self.length*np.sin(self.theta)]
         self.velocity = [self.length*self.omega*np.sin(self.theta), -self.length*self.omega*np.cos(self.theta)]
-        return [self.get_state(), -np.abs(self.target - self.position[0]), False, {}]
+        return [self.get_state(), -i/100, False, {}]
 
     def plot(self):
         x2 = -self.length*np.cos(self.theta)
--- main.py
+++ main.py
@@ -2,31 +2,55 @@ import DERobotArm as gym
 from net import Agent
 from utils import plotLearning
 import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     env = gym.RobotArm()
-    agent = Agent(gamma=0.99, epsilon=1.0, lr=0.01, input_dims=[4], batch_size=64,
-                  n_actions=4, max_mem_size=100000, eps_end=0.01, eps_dec = 5e-4)
+    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=512, input_dims=[4],
+                  n_actions=4, max_mem_size=100000, eps_end=0.0001, eps_dec = 5e-5)
     scores, eps_history = [], []
     n_games = 5000
+    
+    plt.ion()
 
     for i in range(n_games):
+        plt.clf()
+        
         score = 0
-        done = False
         observation = env.reset()
+        
+        done = False
+        plot = i % 100 == 0
+
+        ii = 0
         while not done:
             action = agent.choose_action(observation)
-            observation_, reward, done, info = env.step(action)
+            observation_, reward, done, info = env.step(action, plot, ii)
             score += reward
             agent.store_transition(observation, action, reward,
                                    observation_, done)
             agent.learn()
+            if info == True:
+                done = True
+            
+            if plot:
+                env.plot()
+                plt.scatter(observation[3], -1)
+                plt.title([observation, action])
+                plt.show()
+                plt.pause(0.01)
+                plt.cla()
+                plt.axis([-2,11,-1,2])
+                plt.gca().set_aspect('equal', adjustable='box')
+
             observation = observation_
+            ii += 1
+
+
         scores.append(score)
         eps_history.append(agent.epsilon)
 
         avg_score = np.mean(scores[-100:])
-
         print('episode ', i, 'score %.2f' % score,
               'average score %.2f' % avg_score,
               'epsilon %.2f' % agent.epsilon)
